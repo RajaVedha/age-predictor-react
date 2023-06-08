7876c6a34017dc8ac9e27c0a091988d1cb22a6b8
raja vedha <bandiraja2018@gmail.com>
2023-06-08 20:09:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [name,setName] = useState('');
+  const [predictedAge,setPredictedAge] = useState({});
+
+  const fetchAge = () => {
+     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => setPredictedAge(res.data))
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='headingOne'>Age <span className='headingTwo'>Predictor</span></h1>
+      <div className='container'>
+        <input className="input-field" onChange={(event) => {
+            setName(event.target.value)
+        }}placeholder='Enter Your Name'/>
+        <button className='my-button' onClick={fetchAge}>submit</button>
+      </div> <br /> <br />
+      <div className='container'>
+      {predictedAge.age === null ?  <h1 className='invalidage'> <span className='age'>Invalid</span> Name!</h1>  : <h1 className='validage'>Age : <span className='age'>{predictedAge?.age}</span></h1> }
+      </div>
+     
     </div>
   );
 }
